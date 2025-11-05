69ee7c1ad31c1ba5be00a0ac445084e33a929269
m23lau <mhklau0809@gmail.com>
2025-11-05 15:21:43 -0500

    troubleshooting

--- mcmc.py
+++ mcmc.py
@@ -6,9 +6,44 @@ from emcee import EnsembleSampler
 from corner import corner
 import pickle
 # from scipy.stats import mode
-# import math
+import math
 from multiprocessing import Pool
 from statistics import stdev
+
+
+def corner_plot(flatchain, truths, labels):
+    """ Scans for and takes out stray walkers, then plots a corner plot
+    Args:
+        flatchain (ndarray): 1D or 2D array that feeds into corner plot
+        truths(list: float): List of "True Values" to be highlighted in plot
+        labels (list: str): List of labels for plot
+    Returns:
+         ndarray: Modified flatchain
+    """
+
+    go_away = []
+    # for i in range(len(flatchain)):
+    #     for j in range(len(truths)):
+    #         if not math.isclose(flatchain[i][j], truths[j], rel_tol=1e-3):
+    #             go_away.append(i)
+
+    for i in range(len(flatchain)):
+        if not math.isclose(flatchain[i][0], truths[0], rel_tol=1e-6):
+            go_away.append(i)
+
+    newfc = np.delete(flatchain, go_away, axis=0)
+    print(len(flatchain), len(newfc))
+    print(truths)
+
+    # Plot corner plot before removing strays
+    corner(flatchain, truths=truths, labels=labels)
+    plt.show()
+
+    # Plot corner plot after removing strays, plus print out the lengths of each array that corner.corner takes
+    corner(newfc, truths=truths, labels=labels)
+    plt.show()
+
+    return newfc
 
 
 # Define data points
@@ -20,7 +55,7 @@ ferr = np.abs(flux * (0.00002 * np.random.randn(len(flux))))
 
 
 # Initial guess
-init_p = np.array([56107, 1.81, 89.6, 0.108, 4.08, 0.01, 0.01, np.radians(-3), 0.24, 0.30])
+init_p = np.array([57859.318, 1.81, 89.6, 0.108, 4.08, 0.01, 0.01, np.radians(-3), 0.24, 0.30])
 bounds = [[57855, 57865], [1.8, 1.85], [87.0, 92.0], [0.1, 0.12], [4.0, 4.2], [0.0, 0.02], [0.0, 0.02],
           [-0.1221, 0.0873], [0.1, 0.5], [0.2, 0.5]]
 # soln = minimize(lambda p, t, f, ferr: -log_prob(p, t, f, ferr), init_p, args=(time, flux, ferr), method="Powell")
@@ -30,44 +65,45 @@ p_labels = [r'$t_0$', 'Per', 'Inc', r'$r_p$', 'a', 'q1', 'q2',
             r'$\Delta \phi$', r'$A_B$', r'$C_{11}$']
 
 # Start simulations
-def run_sims(n_dim, init, x, y, yerr, n_burn, n_steps, n_runs, labels):
+def run_sims(init, x, y, yerr, n_burn, n_steps, n_runs, labels):
     """ Run MCMC n_runs times
     Args:
-        n_dim (int): Number of dimensions/parameters
         init (ndarray): List of initial guesses
         x (ndarray): x-values of data points
         y (ndarray): y-values of data points
         yerr (ndarray): Error in y-values of data points
         n_burn (int): Number of burn-in steps
         n_steps (int): Number of steps in each MCMC run
-        n_runs (int): Number of times to run MCMC
+        n_runs (int): Number of times to run burn ins
         labels (list: str): List of labels for corner plot
     Returns:
          tuple: ndarray, ndarray, ndarray
-         last steps of each run, guesses for the last run, flatchain used in corner plot
+         last steps of each run, guesses for the last run, flatchain that will be used in corner plot
     """
-    last_steps = []
-    line = []
-    err = []
-
+    n_dim = len(labels)
     n_walkers = 10 * n_dim
     p0 = [init + 0.001 * np.random.randn(n_dim) for _ in range(n_walkers)]
 
     with Pool() as pool:
         sampler = EnsembleSampler(n_walkers, n_dim, log_prob, args = (x, y, yerr), pool=pool)
-
-        while len(last_steps) < n_runs:
+        count = 0
+        while count < n_runs:
             p1 = sampler.run_mcmc(p0, n_burn, progress = True)      # Burn-in
-            sampler.reset()
-            sampler.run_mcmc(p1, n_steps, progress = True)          # Actual simulation
-            chain = sampler.get_chain()
+            p1_state = p1[0]
 
-            # Set up next run with last step of previous run
-            new_init = np.average(chain[-1], axis=0)
-            last_steps.append(new_init)
-            p0 = [new_init + 0.001 * np.random.randn(n_dim) for _ in range(n_walkers)]
+            # Set up next run with best step of previous run
+            lps = [log_prob(i, x, y, yerr) for i in p1_state]
+            max_lp = max(lps)
+            new_init = p1_state[lps.index(max_lp)]
+            p0 = [new_init + 0.00001 * np.random.randn(n_dim) for _ in range(n_walkers)]
+            sampler.reset()
+            count += 1
+
+        sampler.reset()
+        sampler.run_mcmc(p1, n_steps, progress=True)  # Actual simulation
 
     last_chain = sampler.get_chain()
+    last_steps = np.average(last_chain[-1], axis = 0)
     gs = last_chain.transpose(2, 1, 0)           # Transpose the chain so that each guesses[i] shows each parameter and each
                                                  # walker's steps in a row
 
@@ -84,11 +120,14 @@ def run_sims(n_dim, init, x, y, yerr, n_burn, n_steps, n_runs, labels):
     axs[-1].set_xlabel('Step Number')
 
     # Plot only the last xx% of the steps
-    last_pct = int(n_steps * 0.8)
+    last_pct = int(n_steps * 0.9)
     fig2, axs2 = plt.subplots(n_dim)
     fig2.suptitle('Convergence of Walkers, last ' + str(int((n_steps - last_pct)/n_steps * 100)) + '% of steps')
 
-    skip = 10
+    skip = int(n_steps * 0.01)
+    line = []
+    err = []
+
     for i in range(n_dim):
         line.append(np.average(gs[i], axis=0)[last_pct::skip])         # line has shape (ndim, nsteps/skip)
         gs_t = gs[i].transpose()[last_pct::skip]                       # transpose gs to get all the walkers in a row
@@ -101,7 +140,7 @@ def run_sims(n_dim, init, x, y, yerr, n_burn, n_steps, n_runs, labels):
     xs = np.arange(last_pct, n_steps, skip)
 
     for i in range(len(line)):
-        axs2[i].plot(xs, line[i], color='r')
+        axs2[i].plot(xs, line[i], color = 'r', marker = 'o')
         axs2[i].fill_between(xs, line[i] - err[i], line[i] + err[i], color='r', alpha=0.3)
 
     for k in range(n_dim):
@@ -112,11 +151,18 @@ def run_sims(n_dim, init, x, y, yerr, n_burn, n_steps, n_runs, labels):
     fc = last_chain[last_pct:]
     fc = np.reshape(fc, (len(fc)*n_walkers, n_dim))        # Last xx% of the steps for each walker is saved in fc
 
-    corner(fc, truths=None, labels=labels)
-    plt.show()
+    corner_plot(fc, truths=new_init, labels=labels)
 
     return np.array(last_steps), gs, fc
 
-ls, gs, fc = run_sims(10, init_p, time, flux, ferr, 1000, 1000, 1, p_labels)
+ls, gs, fc = run_sims(init_p, time, flux, ferr, 1000, 5000, 2, p_labels)
 
 
+
+import csv
+with open('file.csv', 'w') as f:
+    writer = csv.writer(f, delimiter = ',')
+    writer.writerow(p_labels)
+    for i in range(len(fc)):
+        writer.writerow([fc[i][0], fc[i][1], fc[i][2], fc[i][3], fc[i][4], fc[i][5], fc[i][6], fc[i][7],
+                         fc[i][8], fc[i][9]])
--- mcmc_funcs.py
+++ mcmc_funcs.py
@@ -15,6 +15,14 @@ def pc_model(params, t):
                                     w = 51, a = semi_a, q = [q1, q2], fp = 1.0, T_s = 6305.79,
                                     rp_a = rad_p / semi_a, limb_dark = 'quadratic', name = 'WASP-76b', channel = f"IRAC {2}",
                                     hotspot_offset = h_off, A_B = b_alb, c11 = c_11)
+
+    # Uncomment these lines to test these three params ONLY (for sanity check)
+    # ttr, pd, rad_p = params
+    # new_t, flux = kelp_models.kelp_transit(t, t0=ttr, per=pd, inc=89.623, rp=rad_p, ecc=0,
+    #                                        w=51, a=4.08, q=[0.01, 0.01], fp=1.0, T_s=6305.79,
+    #                                        rp_a=rad_p / 4.08, limb_dark='quadratic', name='WASP-76b',
+    #                                        channel=f"IRAC {2}",
+    #                                        hotspot_offset=np.radians(-3), A_B=0.2, c11=0.33)
     f_binned = kelp_models.bin(new_t, flux, len(t) + 1)
     return f_binned
 
@@ -28,15 +36,26 @@ def log_prior(params):
     """
     ttr, pd, incl, rad_p, semi_a, q1, q2, h_off, b_alb, c_11 = params
 
-    if (0.00 < q1 < 0.02 and 0.00 < q2 < 0.02
-            and 0.10 < b_alb < 0.50 and 0.20 < c_11 < 0.50):
-        prior_t0 = -0.5 * ((ttr - 56107.35411862118) / 0.000293793706)**2
+    if (57859.31 < ttr < 57859.32 and 1.809 < pd < 1.811 and 89.0 < incl < 90.0
+            and 0.105 < rad_p < 0.11 and 4.05 < semi_a < 4.10 and 0.00 < q1 < 0.02 and 0.00 < q2 < 0.02
+            and np.radians(-10) < h_off < np.radians(5) and 0.10 < b_alb < 0.50 and 0.20 < c_11 < 0.50):
+        prior_t0 = -0.5 * ((ttr - 57859.317) / 0.001)**2
         prior_per = -0.5 * ((pd - 1.80988198) / 0.00000064)**2
         prior_inc = -0.5 * ((incl - 89.623) / 0.034)**2
         prior_rad = -0.5 * ((rad_p - 0.10852) / 0.00096)**2
         prior_a = -0.5 * ((semi_a - 4.08) / 0.06)**2
         prior_offset = -0.5 * ((h_off - np.radians(-3)) / np.radians(10))**2
         return prior_t0 + prior_per + prior_inc + prior_rad + prior_a + prior_offset
+
+    # Uncomment these lines to test these three params ONLY (for sanity check)
+    # ttr, pd, rad_p = params
+    #
+    # if (57859.31 < ttr < 57859.32 and 1.809 < pd < 1.811
+    #         and 0.105 < rad_p < 0.11):
+    #     prior_t0 = -0.5 * ((ttr - 57859.317) / 0.001) ** 2
+    #     prior_per = -0.5 * ((pd - 1.80988198) / 0.00000064)**2
+    #     prior_rad = -0.5 * ((rad_p - 0.10852) / 0.00096)**2
+    #     return prior_t0 + prior_per + prior_rad
 
     else:
         return -np.inf
